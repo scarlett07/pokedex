e321a2ba93a8ba3b921664a30a90a473d48b2e84
scarlett07 <scarlett.geo@gmail.com>
2018-03-07 12:08:23 -0600

    arreglando contenedor

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
+// Variables constantes
 const form = $('#search-form');
 const searcPokemon = $('#search-pkemon');
 const $container = $('#container-pokemons');
+let nombre = $('#nombre')
+
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
 //
 // form.addEventListener('submit', function(e) {
 //   e.preventDefault();
@@ -30,18 +37,26 @@ function drawPokemon(data) {
       let name = element.pokemon_species.name;
       let url = element.pokemon_species.url;
       const imagen = 'https://dummyimage.com/150x150';
-      pokemon += `<li>
-                    <figure>
+      pokemon += `<li class="col l-4">
+                    <figure "col l-4">
                     <img src = ${imagen}>
                     <figcaption>${name}</figcaption>
-                    <span>${url}</span>
+                    <span><a class="waves-effect waves-light btn modal-trigger" href="#modal1">Leer más</a>${url}</span>
                     </figure>
                   </li>`;
-       console.log(pokemon);
+      // console.log(pokemon);
       $container.html(pokemon);
-    //  alert('sa')
+
     }
   })
-}
+};
+
+// $('a').click(function(){
+// 		modal(dataRes);
+//   }
+//
+// function modal(dataRes) {
+// 	$("#nombre").text(dataRes.name);
+// };
 
 getPkemons();
